954b322ef7976a9ecb2ba984174967e88f84d9d5
Albert Sayapov <asayapov@mail.ru>
2022-09-24 16:03:01 +0700

    int to long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        int[] sales = new int[2];
+        long[] sales = new long[2];
         sales[0] = 1680;
         sales[1] = 2260;
         SalesManager salesManager = new SalesManager(sales);
